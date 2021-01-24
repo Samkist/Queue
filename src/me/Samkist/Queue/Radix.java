67a5e7fa29f61cbdea50192b7a17aa7aa788d794
Sam Pizette <samshark23451@gmail.com>
2021-01-24 01:41:23 -0500

    Finished,

--- src/me/Samkist/Queue/Radix.java
+++ src/me/Samkist/Queue/Radix.java
@@ -6,7 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Radix {
     private ArrayList<Queue<String>> radixQueues = new ArrayList<>();
-    private Integer max;
+    private Double max;
 
     public Radix() {
         for(int i = 0; i < 10; i++) {
@@ -17,15 +17,17 @@ public class Radix {
     public void sort(ArrayList<String> nums) {
         findMax(nums);
         zeroFormat(nums);
-        int iterations = Integer.toString(max).length();
+        int iterations = Double.toString(max).length();
         for (int i = 1; i < iterations + 1; i++) {
             int finalI = i;
             radixQueues.forEach(q -> {
                 Iterator<String> it = q.iterator();
                 while (it.hasNext()) {
-                    it.next();
-                    String n = q.dequeue();
-                    radixQueues.get(radixIndex(n, finalI)).enqueue(n);
+                    String n = it.next();
+                    if(radixIndex(n, finalI) >= 0) {
+                        n = q.dequeue();
+                        radixQueues.get(radixIndex(n, finalI)).enqueue(n);
+                    }
                 }
             });
         }
@@ -33,32 +35,36 @@ public class Radix {
     }
 
     private void findMax(ArrayList<String> nums) {
-        max = Integer.parseInt(nums.get(0));
+        max = Double.parseDouble(nums.get(0));
         nums.forEach(s -> {
-            if(Integer.parseInt(s) > max) {
-                max = Integer.parseInt(s);
+            if(Double.parseDouble(s) > max) {
+                max = Double.parseDouble(s);
             }
         });
     }
 
     private int radixIndex(String n, int offset) {
-        return Integer.parseInt(Character.toString(n.charAt(n.length()-offset)));
+        String s = Character.toString(n.charAt(n.length()-offset));
+        if(s.equalsIgnoreCase("."))
+            return -1;
+        return Integer.parseInt(s);
     }
 
     private void zeroFormat(ArrayList<String> nums) {
-        String maxStr = Integer.toString(max);
+        String maxStr = Double.toString(max);
         nums.forEach(n -> {
-            StringBuffer buf = new StringBuffer(n);
+            StringBuffer buf = new StringBuffer(Double.parseDouble(n) + "");
             for(int i = 0; i < maxStr.length()-n.length(); i++) {
                 buf.insert(0, "0");
             }
             n = buf.toString();
-            radixQueues.get(radixIndex(n, 1)).enqueue(n);
+            if(radixIndex(n, 1) >= 0)
+                radixQueues.get(radixIndex(n, 1)).enqueue(n);
         });
     }
 
     private void populateList(ArrayList<String> nums) {
         AtomicInteger index = new AtomicInteger(0);
-        radixQueues.forEach(q -> q.forEach(n -> nums.set(index.getAndIncrement(), Integer.toString(Integer.parseInt(n)))));
+        radixQueues.forEach(q -> q.forEach(n -> nums.set(index.getAndIncrement(), Double.toString(Double.parseDouble(n)))));
     }
 }
